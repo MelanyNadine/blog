726e0ce65405ffcb7a9b3303e7a71cee4c525cc6
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2022-07-04 16:08:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const folders = {
- 	"Onion Sites": "window.addEventListener('load', onions)",
+ 	"Onion Sites": "onions()",
 	"Political Web Articles": "politics()"
 };
 
@@ -29,7 +29,7 @@ function listFolders() {
 	function listElements([key, value]) {
 		let final = key.indexOf(" ");
 		let str = key.slice(0,final);
-		document.getElementById("buttons").innerHTML += '<a href="?'+str+'" onclick="'+value+'"><li>'+key+'</li></a>';
+		document.getElementById("buttons").innerHTML += '<a href="+'+str+'" onclick="'+value+'"><li>'+key+'</li></a>';
   	}
 }
 
