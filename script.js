caa41d2d4f50875289283f46254144255c34c1a0
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2022-07-03 20:21:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const folders = {
 };
 
 const onions = {
-	
+	"Imperial Library of Trantor": "http://kx5thpx2olielkihfyo4jgjqfb7zx7wxr3sd4xzt26ochei4m6f7tayd.onion/",
+	"Onion Index": "http://oniondxjxs2mzjkbz7ldlflenh6huksestjsisc3usxht3wqgk6a62yd.onion",
+	"The Guardian": "http://xp44cagis447k3lpb4wwhcqukix6cgqokbuys24vmxmbzmaq2gjvc2yd.onion",
+	"Relate List": "http://relateoak2hkvdty6ldp7x67hys7pzaeax3hwhidbqkjzva3223jpxqd.onion"
 };
 
 function listFolders() {
@@ -30,4 +33,11 @@ function onions(){
 	folders.style.display = "none";
 	body = document.getElementByTagsName("BODY")[0];
 	body.innerHTML += "<section id='sites'><ul id='list'></ul></section>";
+	let sortedOnions = sortObject(onions);
+	
+	Object.entries(sortedOnions).forEach(listOnions);
+	
+	function listOnions([key, value]) {
+		document.getElementById("list").innerHTML += "<a href='"+value+"'><li>"+key+"</li></a>";
+	}
 }
